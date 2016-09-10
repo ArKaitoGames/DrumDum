d265060d8f3d55bb779eddbedfee3ee2130c63da
JasonIdris <idrisjason@yahoo.com>
2016-09-10 09:57:55 -0400

    Add files via upload

---
+++ Documents/DrumDum/Assets/ButtonClickHandler.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class ButtonClickHandler : MonoBehaviour, IPointerClickHandler {
+
+	private float clickTime;            // time of click
+	public bool onClick = true;            // is click allowed on button?
+	public bool onDoubleClick = false;    // is double-click allowed on button?
+
+	private ClickHandler click;
+
+	void Start() {
+		click = GetComponent<ClickHandler> ();
+	}
+
+	public void OnPointerClick(PointerEventData data)
+	{      
+		int clickCount = 1; // single click
+		
+		// get interval between this click and the previous one (check for double click)
+		float interval = data.clickTime - clickTime;
+		
+		// if this is double click, change click count
+		if (interval < 1.5 && interval > 0)
+			clickCount = 2;
+		
+		// reset click time
+		clickTime = data.clickTime;
+		
+		// single click
+		if (onClick && clickCount == 1)
+		{
+			// enter code here  
+			print ("Clicked");
+		}
+		
+		// double click
+		if (onDoubleClick && clickCount == 2)
+		{
+			// enter code here
+			click.OnMouseDrag();
+		}
+		
+	}
+}
---
+++ Documents/DrumDum/Assets/ClickHandler.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickHandler : MonoBehaviour {
+
+	public GameObject TheDrum;
+
+	public AudioClip theSound;
+	public AudioSource source;
+
+	Touch touch;
+
+	float TouchTime;
+
+	// Use this for initialization
+	void Start () {
+		source = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	 if (Input.GetMouseButtonDown(0)) {
+
+			float vol = Random.Range (0.3f, 1.0f);
+			source.PlayOneShot (theSound, vol);
+
+			/***if (Input.GetMouseButtonUp(0)) {
+				if (Time.time - TouchTime <= 0.5) {
+					//response to tap
+					print ("Tap was" + TouchTime + "seconds long.");
+
+				} else {
+					print ("Long press was" + TouchTime + "seconds long.");
+				}
+			}  ***/
+		}
+		if (Input.GetMouseButtonUp (0)) {
+			source.Stop ();
+		}
+
+    }
+
+	public void OnMouseDrag()
+	{
+		float distance_to_screen = Camera.main.WorldToScreenPoint(TheDrum.transform.position).z;
+		Vector3 pos_move = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen ));
+		transform.position = new Vector3( pos_move.x, transform.position.y, pos_move.z );
+		
+	}
+
+
+	
+}
+
